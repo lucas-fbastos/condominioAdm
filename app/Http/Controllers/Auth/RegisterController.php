7e4f40484909fd11d3d74949ec13568b22a1254e
lucas felix bastos <felixbastos.lucas@gmail.com>
2022-08-05 18:02:58 -0300

    fix: redirect after create

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
